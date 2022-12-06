0cc7c3d4b1ed937a1dad15931b9ed5ac8c516de9
Zackarya Faci <zfaci27@gmail.com>
2022-12-05 21:19:06 -0600

    Day6

---
+++ Day6/Solution2.py
@@ -0,0 +1,19 @@
+f = open("input.txt","r")
+lines = f.readlines()
+line = lines[0]
+
+charSet = set()
+i = 0
+j = 0
+while j < len(line):
+    if len(charSet) == 14:
+        print(j)
+        break
+    elif line[j] in charSet:
+        charSet.remove(line[i])
+        i+=1
+    else: 
+        charSet.add(line[j])
+        j+=1
+
+    
